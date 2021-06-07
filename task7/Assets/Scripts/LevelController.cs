32dfc23e18ca3710e865f6ebad25fff48ca4bbda
chicony2753 <chicony2753@gmail.com>
2021-06-07 12:14:57 +0300

    Menu

--- task7/Assets/Scripts/LevelController.cs
+++ task7/Assets/Scripts/LevelController.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
---
+++ task7/Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void GetLevel()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
